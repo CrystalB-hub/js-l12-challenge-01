ac6d641c6088aa21b6bb30da321c1add5d4adc63
crystal_b <crystal.beecken@gmail.com>
2022-07-13 20:37:30 +0900

    Adding randomIndex function

--- js/script.js
+++ js/script.js
@@ -3,10 +3,18 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-const getimage = async function() {
-    const res = await fetch (https://picsum.photos/v2/list?limit=100);
+const getImage = async function() {
+    const res = await fetch ("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
     console.log(images);
+    selectRandomImage(images);
 };
 
-getimage();
+const selectRandomImage =  function (images){
+    const randomIndex = Math.floor(Math.random() * images.length);
+    console.log(randomIndex);
+};
+
+getImage();
+
+
